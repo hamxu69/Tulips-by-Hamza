ad4cf09afc695b3ede8c3ffdaa9d412a1a73ac84
Hamza-Sama <hamxu69@gmail.com>
2024-12-03 05:51:07 -0800

    Update js.js

--- js.js
+++ js.js
@@ -1,7 +1,6 @@
 window.onload = () => {
     const container = document.querySelector(".container");
   
-    // Check if container exists
     if (!container) {
       console.error("No .container element found!");
       return;
